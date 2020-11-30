c4d3ee14c02abd6fe0738c2803278cef18f54960
mtrimner <matt.trimner@gmail.com>
2020-11-30 10:38:44 -0800

    commit tree 82ca799f18de990e3fb9502b1044abb316d285fa

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    let words = line.split(' ')
+    let capitalWords = words.map( word => word.charAt(0).toUpperCase() + word.slice(1))
+    let newLine = capitalWords.join(' ')
+    return newLine
+  })
 }
